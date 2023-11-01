71a7c6f426e8024a16a823ec4b22a5e5070d5b2b
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2023-11-01 15:10:07 +0400

    [TH2-5117] Added dependency-license-report and gradle-git-properties into base plugin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -15,6 +15,10 @@ repositories {
 dependencies {
     implementation("io.github.gradle-nexus:publish-plugin:1.3.0")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.4")
+    implementation("com.github.jk1.dependency-license-report:com.github.jk1.dependency-license-report.gradle.plugin:2.5")
+    implementation("de.undercouch.download:de.undercouch.download.gradle.plugin:5.5.0")
+    implementation("com.gorylenko.gradle-git-properties:gradle-git-properties:2.4.1")
+
     implementation("org.owasp:dependency-check-gradle:8.4.1")
     // Use the Kotlin JUnit 5 integration.
     testImplementation(gradleTestKit())
--- plugin/src/main/kotlin/com/exactpro/th2/gradle/BaseTh2Plugin.kt
+++ plugin/src/main/kotlin/com/exactpro/th2/gradle/BaseTh2Plugin.kt
@@ -1,5 +1,11 @@
 package com.exactpro.th2.gradle
 
+import com.github.jk1.license.LicenseReportExtension
+import com.github.jk1.license.LicenseReportPlugin
+import com.github.jk1.license.filter.LicenseBundleNormalizer
+import com.github.jk1.license.render.JsonReportRenderer
+import com.gorylenko.GitPropertiesPlugin
+import de.undercouch.gradle.tasks.download.DownloadAction
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPlugin
@@ -9,6 +15,7 @@ import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.withType
 import org.owasp.dependencycheck.gradle.DependencyCheckPlugin
 import org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension
+import java.net.URI
 
 private const val JAVA_VERSION_PROP = "java.version"
 
@@ -24,6 +31,8 @@ class BaseTh2Plugin : Plugin<Project> {
             "th2 base plugin must be applied to the root project"
         }
         configureOwasp(project)
+        configureLicenseReport(project)
+        configureGitProperties(project)
         lockDependencies(project)
         if (project.subprojects.isEmpty()) {
             project.afterEvaluate {
@@ -56,6 +65,30 @@ class BaseTh2Plugin : Plugin<Project> {
                 nodeEnabled = false
             }
         }
+    }
+
+    private fun configureLicenseReport(project: Project) {
+        project.pluginManager.apply(LicenseReportPlugin::class.java)
+        project.extensions.getByType<LicenseReportExtension>().apply {
+            val licenseNormalizerBundlePath = project.buildDir.resolve("license-normalizer-bundle.json")
+
+            if (!licenseNormalizerBundlePath.exists()) {
+                DownloadAction(project).apply {
+                    src("https://raw.githubusercontent.com/th2-net/.github/main/license-compliance/gradle-license-report/license-normalizer-bundle.json")
+                    dest(licenseNormalizerBundlePath)
+                    overwrite(false)
+                }.execute().get()
+            }
+
+            filters = arrayOf(LicenseBundleNormalizer(licenseNormalizerBundlePath.path, false))
+            renderers = arrayOf(JsonReportRenderer("licenses.json", false))
+            excludeOwnGroup = true
+            allowedLicensesFile = URI("https://raw.githubusercontent.com/th2-net/.github/main/license-compliance/gradle-license-report/allowed-licenses.json")
+        }
+    }
+
+    private fun configureGitProperties(project: Project) {
+        project.pluginManager.apply(GitPropertiesPlugin::class.java)
     }
 
     private fun Project.configureManifestInfo() {
